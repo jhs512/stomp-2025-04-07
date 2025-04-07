0bea8f1dd45661e3f2c94a1c0085b626b1b1db5a
jhs512 <jangka512@gmail.com>
2025-04-07 09:07:34 +0900

    work

---
+++ src/main/java/com/back/Stomp20250407Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stomp20250407Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stomp20250407Application.class, args);
+    }
+
+}
