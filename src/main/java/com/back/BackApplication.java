0ccdee533b3e9f374c96a8ed518b099131735a1a
jhs512 <jangka512@gmail.com>
2025-04-07 09:09:46 +0900

    BackApplication 으로 변경

--- src/main/java/com/back/BackApplication.java
+++ src/main/java/com/back/BackApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Stomp20250407Application {
+public class BackApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Stomp20250407Application.class, args);
+        SpringApplication.run(BackApplication.class, args);
     }
 
 }
